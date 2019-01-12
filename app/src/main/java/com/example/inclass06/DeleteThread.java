fd0ae1799750f11784e181b011a2aa05cdceb114
Manali Ghare <manalighare19@gmail.com>
2019-01-12 18:42:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/inclass06/DeleteThread.java
@@ -0,0 +1,15 @@
+package com.example.inclass06;
+
+
+public class DeleteThread {
+
+    String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
